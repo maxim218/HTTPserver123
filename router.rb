2e64c841716d48019b08ef03b092f9ecb10c6611
maxim218 <maxim218@yandex.ru>
2018-09-17 16:28:28 +0300

    Many tests passed

--- router.rb
+++ router.rb
@@ -18,6 +18,18 @@ def give_file (url, pairs_array, session, file_name, file_type)
   if file_type == "PNG"
     manager.add_headers_string "Content-Type: image/png"
   end
+  if file_type == "GIF"
+    manager.add_headers_string "Content-Type: image/gif"
+  end
+  if file_type == "JPG"
+    manager.add_headers_string "Content-Type: image/jpeg"
+  end
+  if file_type == "JS"
+    manager.add_headers_string "Content-Type: text/javascript"
+  end
+  if file_type == "SWF"
+    manager.add_headers_string "Content-Type: application/x-shockwave-flash"
+  end
   manager.add_headers_string "Content-Length: " + f_length.to_s
   print "Size: " + f_length.to_s + "\n"
   result = manager.get_full_result
@@ -45,12 +57,41 @@ def route_query (url, pairs_array, session)
     type = (arr[arr.length-1] + "").to_s
     if type == "html"
       give_file url, pairs_array, session, file_name.to_s, "HTML"
+      return
     end
     if type == "css"
       give_file url, pairs_array, session, file_name.to_s, "CSS"
+      return
     end
     if type == "png"
       give_file url, pairs_array, session, file_name.to_s, "PNG"
+      return
     end
+    if type == "gif"
+      give_file url, pairs_array, session, file_name.to_s, "GIF"
+      return
+    end
+    if type == "jpg"
+      give_file url, pairs_array, session, file_name.to_s, "JPG"
+      return
+    end
+    if type == "jpeg"
+      give_file url, pairs_array, session, file_name.to_s, "JPG"
+      return
+    end
+    if type == "js"
+      give_file url, pairs_array, session, file_name.to_s, "JS"
+      return
+    end
+    if type == "swf"
+      give_file url, pairs_array, session, file_name.to_s, "SWF"
+      return
+    end
+  else
+    manager = Session_manager.new
+    manager.init_fields
+    manager.set_http_type_string "HTTP/1.1 404"
+    result = manager.get_full_result
+    session.print result.to_s
   end
 end
